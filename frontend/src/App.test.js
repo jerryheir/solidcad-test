6185c488d1925d2e703160078148de0aa13c1e38
Jerry Nwaeze <nwaezejerry@gmail.com>
2022-11-20 02:13:36 +0100

    Implement simple frontend tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders contact form text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/contact form/i);
   expect(linkElement).toBeInTheDocument();
 });
