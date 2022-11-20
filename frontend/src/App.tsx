12ab0e5acfa1c7cd3926ba5204130fba0e8972e2
Jerry Nwaeze <nwaezejerry@gmail.com>
2022-11-20 01:01:43 +0100

    Complete logic and locking versions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import Input from "components/Input";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import * as Yup from "yup";
 import { useFormik } from "formik";
+import Button from "components/Button";
 
 const Container = styled.div`
   display: flex;
@@ -21,14 +22,32 @@ const HeaderText = styled.p`
   text-align: center;
 `;
 
+const PText = styled.p`
+  font-size: 12px;
+  font-style: italic;
+  align-self: center;
+  text-align: center;
+  margin: 11px;
+`;
+
+const contactSchema = Yup.object().shape({
+  firstname: Yup.string()
+    .required("Required")
+    .max(25, "Maximum of 25 characters"),
+  lastname: Yup.string()
+    .required("Required")
+    .max(25, "Maximum of 25 characters"),
+  email: Yup.string()
+    .required("Email is Required")
+    .email("Invalid email")
+    .max(50, "Maximum of 50 characters"),
+  message: Yup.string()
+    .required("Message is required.")
+    .max(500, "Maximum of 500 characters"),
+});
+
 const App = () => {
-  const contactSchema = Yup.object().shape({
-    firstname: Yup.string().required("Required").max(25, 'Maximum of 25 characters'),
-    lastname: Yup.string().required("Required").max(25, 'Maximum of 25 characters'),
-    email: Yup.string().required("Email is Required").email("Invalid email").max(50, 'Maximum of 50 characters'),
-    message: Yup.string()
-      .required("Message is required.").max(500, 'Maximum of 500 characters')
-  });
+  const [message, setMessage] = useState("");
   const formik = useFormik({
     initialValues: {
       firstname: "",
@@ -36,17 +55,51 @@ const App = () => {
       email: "",
       message: "",
     },
-    onSubmit: async (values) => {},
+    onSubmit: async (values) => {
+      setMessage("");
+      try {
+        await fetch("http://localhost:8080/contact", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify(values),
+        });
+        setMessage("Contact successfully added");
+        formik.resetForm();
+      } catch (e) {
+        setMessage("Error: Network Connection failed");
+      }
+    },
     validationSchema: contactSchema,
   });
   return (
     <Container>
       <HeaderText>Test App</HeaderText>
-      <Input 
-      
+      <Input
+        placeholder={"First Name"}
+        value={formik.values.firstname}
+        onChange={formik.handleChange("firstname")}
       />
+      <Input
+        placeholder={"Last Name"}
+        value={formik.values.lastname}
+        onChange={formik.handleChange("lastname")}
+      />
+      <Input
+        placeholder={"Email"}
+        value={formik.values.email}
+        onChange={formik.handleChange("email")}
+      />
+      <Input
+        placeholder={"Message"}
+        value={formik.values.email}
+        onChange={formik.handleChange("message")}
+      />
+      <PText>{message}</PText>
+      <Button text={"COMPLETE"} onClick={formik.handleSubmit} />
     </Container>
   );
-}
+};
 
 export default App;
