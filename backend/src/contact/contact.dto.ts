4150f9cf0b874f28e9bf9a8ed8b7c47b6a6de1cf
Jerry Nwaeze <nwaezejerry@gmail.com>
2022-11-21 19:14:37 +0100

    removed duplicate lock files

--- backend/src/contact/contact.dto.ts
+++ backend/src/contact/contact.dto.ts
@@ -1,19 +1,23 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateContactDto {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(25)
   firstname: string;
 
   @IsNotEmpty()
   @IsString()
+  @MaxLength(25)
   lastname: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @MaxLength(50)
   email: string;
 
   @IsNotEmpty()
   @IsString()
+  @MaxLength(500)
   message: string;
 }
